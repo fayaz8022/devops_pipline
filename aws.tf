bb572dae890c20460606e3adf40683c51fada209
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-02-16 17:32:32 +0530

    security group

--- aws.tf
+++ aws.tf
@@ -1,7 +1,35 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
+#Creating security groups and allow ssh, http and custom icmp
+resource "aws_security_group" "terraform_sg" {
+name = "terraform_sg"
+description = "allow ssh, http and cutom icmp traffic"
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port = 8080
+to_port = 8080
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
 resource "aws_instance" "testterrafrom" {
      ami = "ami-08e0ca9924195beba"
   instance_type = "t2.micro"
