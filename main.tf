5c0fe3c6ea91fe0d20e056ad1c568b3f93fbd996
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-02-19 14:31:58 +0530

    with s3 and update

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "mywalfile"
+  acl    = "private"
+
+  tags = {
+    Name        = "mywarfile"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_instance" "testterrafrom" {
+     ami = "ami-08e0ca9924195beba"
+    instance_type = "t2.micro"
+    key_name = "devops" 
+   user_data = <<-EOF
+             #!/bin/bash
+             yum install java -y
+             yum install wget -y
+             cd /opt/
+             wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.43/bin/apache-tomcat-9.0.43.tar.gz
+             tar -xvf apache-tomcat-9.0.43.tar.gz
+             ./apache-tomcat-9.0.43/bin/catalina.sh run
+             yum install git -y
+             git clone https://github.com/fayaz8022/devopsdemo.git
+             EOF
+  tags = {
+    Name = "tomcat_container"
+  }
+}
