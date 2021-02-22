1d8edce8fc095cd143a8006f8635d8500d6d00f2
fayaz8022 <46531802+fayaz8022@users.noreply.github.com>
2021-02-22 13:39:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "b" {
 resource "aws_instance" "testterrafrom" {
      ami = "ami-08e0ca9924195beba"
     instance_type = "t2.micro"
-    key_name = "devops" 
+    key_name = "pipline" 
    user_data = <<-EOF
              #!/bin/bash
              yum install java -y
