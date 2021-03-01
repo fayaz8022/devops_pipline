0440c5c4be61d9994f855d6a779d04cf34e5ac6c
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-03-01 11:02:46 +0530

    testing sonarqube

--- src/main/java/devops/pipeline/PipelineApplication.java
+++ src/main/java/devops/pipeline/PipelineApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PipelineApplication {
-
+this  is to test sonarqube
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
 	}
